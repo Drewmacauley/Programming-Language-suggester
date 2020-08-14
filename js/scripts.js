7645e8b3074ee054156e0995a25618e17e25750d
Drewmacauley <macauleyandy03@gmail.com>
2020-08-14 11:01:27 -0700

    add function to JS

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document ).ready(function() {
+  $("form#personInfo").submit(function(event) {
+const gems = $("select#gems").val();
+const grade = $("select#grade").val();
+const go = $("select#go").val();
+const snake = $("selct#snake").val();
+const color = $("select#color").val();
+
+  }
+});
