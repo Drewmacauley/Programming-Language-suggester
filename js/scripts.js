5c76a1aa9363359729dea7914f048de4d69b13ec
Drewmacauley <macauleyandy03@gmail.com>
2020-08-29 12:06:30 -0700

    add more scripts/ debugging/update README

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   $('form#personInfo').submit(function (event) {
-    const grade = $('select#grade').val()
-    const go = $('select#go').val()
-    const snake = $('select#snake').val()
-
+    const grade = $('select#grade').val();
+    const go = $('select#go').val();
+    const snake = $('select#snake').val();
     if (grade === 'a') {
       if (go === 'goS') {
         if (snake === 'cobra') {
@@ -31,7 +30,7 @@ $(document).ready(function () {
         }
       }
     } else if (grade === 'c') {
-      if (go === 'yeild') {
+      if (go === 'goS') {
         if (snake === 'cobra') {
           $('#code').text('ruby');
         } else if (snake === 'viper') {
@@ -39,14 +38,30 @@ $(document).ready(function () {
         } else if (snake === 'viper') {
           $('#code').text('swift');
         }
+      } else if (go === 'stop') {
+        if (snake === 'cobra') {
+          $('#code').text('javascript');
+        } else if (snake === 'python') {
+          $('#code').text('python');
+        } else if (snake === 'viper') {
+          $('#code').text('swift');
+        }
+      } else if (go === 'yeild') {
+        if (snake === 'cobra') {
+          $('#code').text('javascript');
+        } else if (snake === 'python') {
+          $('#code').text('python');
+        } else if (snake === 'viper') {
+          $('#code').text('swift');
+        }
       }
-    } else if (go === 'goS') {
-      if (grade === 'c') {
+    } else if (grade === 'b') {
+      if (go === 'goS') {
         if (snake === 'cobra') {
           $('#code').text('ruby');
         } else if (snake === 'viper') {
           $('#code').text('ruby');
-        } else if (snake === 'viper') {
+        } else if (snake === 'python') {
           $('#code').text('swift');
         }
       } else if (go === 'stop') {
@@ -66,18 +81,8 @@ $(document).ready(function () {
           $('#code').text('javascript');
         }
       }
-    } else if (grade === 'b') {
-      if (snake === 'viper') {
-        $('#code').text('swift');
-      } else if (snake === 'cobra') {
-        $('#code').text('ruby');
-      } else if (snake === 'python') {
-        $('#code').text('javascript');
-      }
     } 
-
     $('#result').show();
-
     event.preventDefault();
-  })
-})
+  });
+});
